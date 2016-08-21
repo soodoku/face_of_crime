f8f30381f383b17c08a24bd4c94bbab3aa7d3c1b
***** <gsood07@gmail.com>
2016-08-21 11:40:41 -0400

    lit. review table

---
+++ scripts/05_lit.R
@@ -0,0 +1,24 @@
+"
+Include Lit. Review Table
+
+"
+
+# Set working dir.
+setwd(githubdir)
+setwd("face_of_crime/")
+
+# Load libs
+library(xtable)
+
+# Read in data
+lit <- read.csv("data/lit.csv")
+
+print(
+	  xtable(lit, 
+	  	caption="Racial Distribution of Perpetrators and Victims on Various Television Shows", 
+	  	align= c("p{0.04\\textwidth}", "p{0.1\\textwidth}", "p{0.1\\textwidth}", "p{0.1\\textwidth}", "p{0.55\\textwidth}", "p{0.12\\textwidth}"), label="tab:lit"), 
+		include.rownames = FALSE,
+	    include.colnames = TRUE, size="\\tiny", 
+	    type = "latex", sanitize.text.function = function(x){x},
+	    caption.placement="top",table.placement="!htb",
+	    file="tabs/lit.tex")
